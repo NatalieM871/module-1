ff0478aea1e6cdf974bb6ff149ed59a650df750b
NatalieM871 <natalie_m118@yahoo.com>
2024-07-31 01:55:20 +1000

    Add files via upload

---
+++ module1_2.py
@@ -0,0 +1,5 @@
+number_of_tasks = 12
+hours_spent = 1.5
+nameOfCourse = 'Python'
+hours_per_task = 1.5 / 12
+print("Курс: ", nameOfCourse, ", всего задач: ", number_of_tasks, ", затрачено часов: ", hours_spent, ", среднее время выполнения ", hours_per_task, "часа.")
